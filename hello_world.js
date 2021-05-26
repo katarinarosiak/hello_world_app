0d9d2944398b03cdb79d6306ac12549abe07c572
katarina <katarinarosiak@gmail.com>
2021-05-26 12:29:31 +0200

    final commit

--- hello_world.js
+++ hello_world.js
@@ -2,6 +2,33 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+const COUNTRY_DATA = [
+  {
+    path: "/english",
+    flag: "flag-of-United-States-of-America.png",
+    alt: "US Flag",
+    title: "Go to US English site",
+  },
+  {
+    path: "/french",
+    flag: "flag-of-France.png",
+    alt: "Drapeau de la france",
+    title: "Aller sur le site français",
+  },
+  {
+    path: "/serbian",
+    flag: "flag-of-Serbia.png",
+    alt: "Застава Србије",
+    title: "Идите на српски сајт",
+  },
+];
+
+const LANGUAGE_CODES = {
+  english: "en-US",
+  french: "fr-FR",
+  serbian: "sr-Cryl-rs",
+};
+
 
 //where to look for pug file, use pug engine
 app.set('views', './views');
@@ -13,24 +40,34 @@ app.use(express.static('public')); //where to look for static assets
 //loging status
 app.use(morgan('common'));
 
+//view helper - property of app.local. 
+app.locals.currentPathClass = (path, currentPath) => {
+  return path === currentPath ? "current" : "";
+};
+
 //ROUTING
 //render pug file
 app.get('/', (req, res) => {
   res.redirect("/english");
 });
 
-app.get("/english", (req, res) => {
-  res.render("hello-world-english");
+app.get("/:language", (req, res, next) => {
+  const language = req.params.language;
+  const languageCode = LANGUAGE_CODES[language];
+  if (!languageCode) {
+    next(new Error(`Language not supported: ${language}`));
+  } else {
+    res.render(`hello-world-${language}`, {
+      countries: COUNTRY_DATA,
+      currentPath: req.path,
+      language: languageCode,
+    });
+  }
+})
+app.use((err, req, res, _next) => {
+  console.log(err);
+  res.status(404).send(err.message);
 });
-
-app.get("/french", (req, res) => {
-  res.render("hello-world-french");
-});
-
-app.get("/serbian", (req, res) => {
-  res.render("hello-world-serbian");
-});
-
 
 app.listen(3000, 'localhost', () => {
   console.log('Listening to port 3000');
